4fb1da9cba2f6a1e157ddf53e091efbbd73f0963
Thomas Domingo <tdomingo90@gmail.com>
2020-12-04 17:06:09 -0800

    make messageView append messages to chats

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -1,12 +1,35 @@
 var MessageView = {
-
   render: _.template(`
-      <!--
-      <div class="chat">
-        <div class="username"></div>
-        <div></div>
-      </div>
-      -->
-    `)
+
+  <div class="<%- roomname %> chat" >
+  <div class="username"><%- username %></div>
+  <div class="message"><%- text %></div>
+  </div>
+
+  `),
+  //displayNewMessage(object) {
+  display : function (obj) {
+  //1 render(object)
+    var rendered = '';
+    rendered += MessageView.render(obj);
+    //2 append
+    $('#chats').append(rendered);
+
+  }
+};
+  //render(user, text, room) ==> returns HTML node matching template's style
+  //append to the div with id: chats
+
+//   var template = _.template("<b><%- value %></b>");
+//   template({value: '<script>'});
+//  console => "<b>&lt;script&gt;</b>"
+
+
+//$(() => App.initialize())
+var message = {
+  username: 'shawndrost',
+  text: 'THANK YOU',
+  roomname: '4chan'
 
 };
+MessageView.display(message);
